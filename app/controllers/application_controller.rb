a4bf585ca598a17fe332205dec08323b1be36ca0
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-06-25 20:53:18 +0300

    Fix Linter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,9 +9,7 @@ class ApplicationController < ActionController::API
     begin
       @decoded = JsonWebToken.decode(header)
       @current_user = User.find(@decoded[:user_id])
-    rescue ActiveRecord::RecordNotFound => e
-      render json: { errors: e.message }, status: :unauthorized
-    rescue JWT::DecodeError => e
+    rescue StandardError => e
       render json: { errors: e.message }, status: :unauthorized
     end
   end
