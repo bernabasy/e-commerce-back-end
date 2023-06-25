665b91ab7d8dda908efae0b79e23b82a4c2a751f
bernabasy <104406349+bernabasy@users.noreply.github.com>
2023-06-25 20:26:52 +0300

    Update routes for Jwt

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+    resources :users, param: :_username
+    post '/auth/login', to: 'authentication#login'
+    get '/*a', to: 'application#not_found'
 end
